ccd078aae969eaee049444b53b04f88680770a36
qLepeshka <qlepeshka@gmail.com>
2025-09-03 15:13:19 +0300

    trash

---
+++ ShopApi/Hubs/OrderHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ShopApi.Hubs
+{
+    public class OrderHub : Hub
+    {
+        public async Task JoinAdminGroup()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "Admins");
+        }
+    }
+}
